8b65ffaecdcb5e315fa4655cfe3d6d37819faa95
I-Satyam-Shandhilya <106227195+I-Satyam-Shandhilya@users.noreply.github.com>
2023-11-27 17:27:57 +0530

    added atlas

--- src/mongo.js
+++ src/mongo.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/LoginSignUpForm")
+mongoose.connect("mongodb+srv://satyam:Pepsodent@recipes.koudlpl.mongodb.net/LoginSignUpForm")
 .then(()=>{
     console.log('mongoose connected');
 })
